07b78963c3045e3b280f4a5e09f660194768a0da
adityasingh1105 <adisadis1234@gmail.com>
2022-06-28 14:57:44 +0530

    app.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Component/NavBar";
+import NewsItem from "./Component/NewsItem";
+import { Component } from "react";
+import News from "./Component/News";
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <div>
+          <Navbar />
+          <News/>
+          
+        </div>
+      </>
+    );
+  }
+}
